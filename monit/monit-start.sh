7d68a0ef8ce0b27dcc10dc08194d32558e2805bb
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2016-10-11 14:10:12 +0200

    Monit in a Container

---
+++ monit/monit-start.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+MONIT_ARGS=${MONIT_ARGS:-"-I"}
+
+if [ ! -d "${MONIT_HOME}/monitrc.d" ]; then
+	mkdir ${MONIT_HOME}/monitrc.d
+  cat <<-EOF > ${MONIT_HOME}/monitrc.d/basic
+  set daemon 60
+
+  set httpd port 2812
+    allow localhost
+    allow test:test
+
+  set mmonit ${MONIT_URL}
+EOF
+fi
+
+for i in "${MONIT_LOG}/monit.pid ${MONIT_LOG}/monit.state"
+do
+	if [ -e "$i" ]; then
+		rm "$i"
+	fi
+done
+
+trap 'kill -SIGTERM $PID' SIGTERM SIGINT
+monit ${MONIT_ARGS} &
+PID=$!
+wait $PID
