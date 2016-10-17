4ec90aff00938d0661cff9cd69f8bdee382454df
Marcel Grossmann <whatever4711@gmail.com>
2016-10-17 09:52:52 +0200

    Updates on Postgres

--- postgres/docker-entrypoint.sh
+++ postgres/docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set_listen_addresses() {
+	sedEscapedValue="$(echo "$1" | sed 's/[\/&]/\\&/g')"
+	sed -ri "s/^#?(listen_addresses\s*=\s*)\S+/\1'$sedEscapedValue'/" "$PGDATA/postgresql.conf"
+}
+
 set -e
 
 if [ "${1:0:1}" = '-' ]; then
@@ -25,16 +30,16 @@ if [ "$1" = 'postgres' ]; then
 		else
 			# The - option suppresses leading tabs but *not* spaces. :)
 			cat >&2 <<-'EOWARN'
-				****************************************************
-				WARNING: No password has been set for the database.
-				         This will allow anyone with access to the
-				         Postgres port to access your database. In
-				         Docker's default configuration, this is
-				         effectively any other container on the same
-				         system.
-				         Use "-e POSTGRES_PASSWORD=password" to set
-				         it in "docker run".
-				****************************************************
+			****************************************************
+			WARNING: No password has been set for the database.
+			This will allow anyone with access to the
+			Postgres port to access your database. In
+			Docker's default configuration, this is
+			effectively any other container on the same
+			system.
+			Use "-e POSTGRES_PASSWORD=password" to set
+			it in "docker run".
+			****************************************************
 			EOWARN
 
 			pass=
@@ -43,56 +48,56 @@ if [ "$1" = 'postgres' ]; then
 
 		{ echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
 
-		# internal start of server in order to allow set-up using psql-client
-		# does not listen on external TCP/IP and waits until start finishes
-		gosu postgres pg_ctl -D "$PGDATA" \
-			-o "-c listen_addresses='localhost'" \
-			-w start
+			# internal start of server in order to allow set-up using psql-client
+			# does not listen on external TCP/IP and waits until start finishes
+			gosu postgres pg_ctl -D "$PGDATA" \
+				-o "-c listen_addresses='localhost'" \
+				-w start
 
-		: ${POSTGRES_USER:=postgres}
-		: ${POSTGRES_DB:=$POSTGRES_USER}
-		export POSTGRES_USER POSTGRES_DB
+			: ${POSTGRES_USER:=postgres}
+			: ${POSTGRES_DB:=$POSTGRES_USER}
+			export POSTGRES_USER POSTGRES_DB
 
-		psql=( psql -v ON_ERROR_STOP=1 )
+			psql=( psql -v ON_ERROR_STOP=1 )
 
-		if [ "$POSTGRES_DB" != 'postgres' ]; then
-			"${psql[@]}" --username postgres <<-EOSQL
+			if [ "$POSTGRES_DB" != 'postgres' ]; then
+				"${psql[@]}" --username postgres <<-EOSQL
 				CREATE DATABASE "$POSTGRES_DB" ;
+				EOSQL
+				echo
+			fi
+
+			if [ "$POSTGRES_USER" = 'postgres' ]; then
+				op='ALTER'
+			else
+				op='CREATE'
+			fi
+			"${psql[@]}" --username postgres <<-EOSQL
+			$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
 			EOSQL
 			echo
-		fi
 
-		if [ "$POSTGRES_USER" = 'postgres' ]; then
-			op='ALTER'
-		else
-			op='CREATE'
-		fi
-		"${psql[@]}" --username postgres <<-EOSQL
-			$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
-		EOSQL
-		echo
+			psql+=( --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" )
 
-		psql+=( --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" )
-
-		echo
-		for f in /docker-entrypoint-initdb.d/*; do
-			case "$f" in
-				*.sh)     echo "$0: running $f"; . "$f" ;;
-				*.sql)    echo "$0: running $f"; "${psql[@]}" < "$f"; echo ;;
-				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
-				*)        echo "$0: ignoring $f" ;;
-			esac
 			echo
-		done
+			for f in /docker-entrypoint-initdb.d/*; do
+				case "$f" in
+					*.sh)     echo "$0: running $f"; . "$f" ;;
+					*.sql)    echo "$0: running $f"; "${psql[@]}" < "$f"; echo ;;
+					*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
+					*)        echo "$0: ignoring $f" ;;
+				esac
+				echo
+			done
 
-		gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+			gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+			set_listen_addresses '*'
+			echo
+			echo 'PostgreSQL init process complete; ready for start up.'
+			echo
+		fi
 
-		echo
-		echo 'PostgreSQL init process complete; ready for start up.'
-		echo
+		exec gosu postgres "$@"
 	fi
 
-	exec gosu postgres "$@"
-fi
-
-exec "$@"
+	exec "$@"
