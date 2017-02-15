33b3834f25d5f658aa66be6defefe66d1b5f73d2
clemensk <git@clemensklug.de>
2017-02-15 20:46:56 +0100

    replace IP by containername

--- Rplots/postgres.r
+++ Rplots/postgres.r
@@ -7,7 +7,7 @@ require("sqldf")
 require("jsonlite")
 
 
-monit_db <- function(host="172.20.0.2", port=5432, dbname="pymonit", user="postgres", password="postgres"){
+monit_db <- function(host="db", port=5432, dbname="pymonit", user="postgres", password="postgres"){
   driver <- dbDriver("PostgreSQL")
   connection <- dbConnect(driver, dbname=dbname, host=host, port=port, user=user, password=password)
   return(connection)
