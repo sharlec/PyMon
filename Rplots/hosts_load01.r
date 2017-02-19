9ee19c4140cffce53db45d033cf1ec39510873d1
clemensk <git@clemensklug.de>
2017-02-19 18:29:20 +0100

    improve x-axis

--- Rplots/hosts_load01.r
+++ Rplots/hosts_load01.r
@@ -14,11 +14,11 @@ prepare_load_data01 = function(sys, i){
 
 plot_load01 = function(sys, i, x){
 	lines(prepare_load_data01(sys, i), col=i, )
-	axis.POSIXct(1,as.POSIXct(fromJSON(systems$date[i]), origin="1970-01-01"), format="%Y-%m-%d %H:%M:%S")
 }
 
 png(file="hosts_load01.png")
 plot(c(start,end),c(0,10),type="n", xlab="Time",ylab="Load (Avg 1 min)")
+axis.POSIXct(1,as.POSIXct(seq(start,end), origin="1970-01-01"), format="%Y-%m-%d %H:%M:%S")
 
 lapply(seq(1,length(systems$server_id)), function(i) plot_load01(systems, i , data.frame(systems$server_id[i])))
 legend("topleft", legend=systems$localhostname, lty=c(1,1), col=c(seq(1, length(systems$server_id))))
