59189427e3d91d2fa2a2ef7fdde2744145fe55b5
clemensk <git@clemensklug.de>
2017-02-19 19:29:00 +0100

    refactor hosts_load01.r to enable all kind of plots

--- Rplots/containers_time.r
+++ Rplots/containers_time.r
@@ -4,3 +4,11 @@ source("db.r")
 #print(systems)
 raw = sqldf(paste(readLines("containers_time.sql"),collapse=" "), connection=connection)
 
+add_timestamp = function(containers, host, time){
+	
+
+plot_containers = function(i, x, raw, containers){
+	lapply(fromJSON(raw$date), function(timestamp) 
+}
+containers = data.frame()
+lapply(seq(1, length(raw$localhostname)), function(i) plot_containers(i, data.frame(raw$localhostname[i]), raw, containers))
--- Rplots/hosts_load01.r
+++ Rplots/hosts_load01.r
@@ -6,21 +6,27 @@ systems <- sqldf("select monitcollector_server.localhostname,monitcollector_syst
 # Date -> unix: as.numeric(as.POSIXct("2017-02-19 17:51:17 CET"))
 start = as.numeric(as.POSIXct("2017-02-06 17:53:20 CET"))
 end = as.numeric(as.POSIXct("2017-02-07 21:40:00 CET"))
-prepare_load_data01 = function(sys, i){
-	sysdf = data.frame(date=fromJSON(sys$date[i]), load=fromJSON(sys$load_avg01[i]))
+prepare_load_data01 = function(sys, i, field){
+	sysdf = data.frame(date=fromJSON(sys$date[i]), load=fromJSON(sys[[field]][i]))
 	sysdfLate = subset(x=sysdf, subset= date>start & date<end)
 	return(sysdfLate)
 }
 
-plot_load01 = function(sys, i, x){
-	lines(prepare_load_data01(sys, i), col=i, )
+plot_load01 = function(sys, i, field, x){
+	lines(prepare_load_data01(sys, i, field), col=i)
 }
 
-png(file="hosts_load01.png")
-plot(c(start,end),c(0,10),type="n", xlab="Time",ylab="Load (Avg 1 min)")
-axis.POSIXct(1,as.POSIXct(seq(start,end), origin="1970-01-01"), format="%Y-%m-%d %H:%M:%S")
+plot_system = function (file, start, end, ymin, ymax, xlab, ylab, sys, field, plot_fun){
+	png(file=file)
+	plot(c(start,end), c(ymin, ymax), type="n", xlab=xlab, ylab=ylab)
+	axis.POSIXct(1,as.POSIXct(seq(start,end), origin="1970-01-01"), format="%Y-%m-%d %H:%M:%S")
+	lapply(
+		seq(1,length(sys$server_id)), 
+		function(i) plot_fun(sys, i, field, data.frame(systems$server_id[i]))
+	)
+	legend("topleft", legend=systems$localhostname, lty=c(1,1), col=c(seq(1, length(systems$server_id))))
+	dev.off()
+}
 
-lapply(seq(1,length(systems$server_id)), function(i) plot_load01(systems, i , data.frame(systems$server_id[i])))
-legend("topleft", legend=systems$localhostname, lty=c(1,1), col=c(seq(1, length(systems$server_id))))
+plot_system("hosts_load01.png", start, end, 0, 10, "Time", "Load (avg 1 min)", systems, "load_avg01", plot_load01)
 
-dev.off()
